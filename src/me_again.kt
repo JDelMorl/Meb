6814ebe4401b5e961ea10bfa36cbcda3775e6d43
Jesus Delmoral <jdelmoral@students.mchenry.edu>
2020-08-25 11:07:46 -0500

    Initial commit

---
+++ src/me_again.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("I like to play the game League of Legends")
+    println("Ive started playing the Yakuza (series) games, probably my favorite game franchise now")
+    println("I am 20 turning 21 in December")
+    println("anime is cool, i like a lot")
+}
